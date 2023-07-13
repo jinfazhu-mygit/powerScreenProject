db29cfbfc9325a25fe7a8635a564b93ae497d0c9
jinfazhu-mygit <2095229306@qq.com>
2023-07-13 22:33:20 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'./',// 将根路径换成相对路径
+  base:'.',// 将根路径换成相对路径
   plugins: [
     vue(),
   ],
